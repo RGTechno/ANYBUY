24c5fb046181b2c578edd25df57fee5361776b94
Rahul Gandhi <65856344+RGTechno@users.noreply.github.com>
2021-03-29 14:37:56 +0530

    cat icons

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:anybuy/screens/Home_Screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,6 +9,9 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'AnyBuy',
--- lib/widgets/Categories.dart
+++ lib/widgets/Categories.dart
@@ -3,14 +3,42 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class Categories extends StatelessWidget {
-  final List<Map<String, dynamic>> catMap = [
-    {"color": Colors.tealAccent, "text": "Restaurants"},
-    {"color": Colors.pinkAccent, "text": "Stationaries"},
-    {"color": Colors.purpleAccent, "text": "Grocery"},
-    {"color": Colors.redAccent, "text": "Medicines"},
-    {"color": Colors.orangeAccent, "text": "Hypermarts"},
-    {"color": Colors.yellow, "text": "Dairy"},
-    {"color": Colors.lightGreenAccent, "text": "Sports"},
+  final List<Map<String, String>> catMap = [
+    {
+      "image":
+          "https://www.pngkit.com/png/detail/366-3665671_restaurant-meal-food-clipart-transparent-background.png",
+      "text": "Restaurants"
+    },
+    {
+      "image":
+          "https://library.kissclipart.com/20190916/wgq/kissclipart-clip-art-toy-stationery-f3217c38ff505a8b.png",
+      "text": "Stationaries"
+    },
+    {
+      "image":
+          "https://www.clipartkey.com/mpngs/m/60-609806_transparent-grocery-store-clip-art.png",
+      "text": "Grocery"
+    },
+    {
+      "image":
+          "https://lh3.googleusercontent.com/proxy/_tjMe5xF7ii5Jsw1Cu8cwOaAJkqQ2SU-Mt9diDzF25ZnmqpOOok3iTo_kr7yxhPmAPNA_I9lbzP4Q0kj1nkkcLJa4kmi1tCp1Q",
+      "text": "Medicines"
+    },
+    {
+      "image":
+          "https://thumbs.dreamstime.com/b/supermarket-facade-people-shopping-product-hypermarket-grocery-food-store-male-female-buyers-vector-background-153201502.jpg",
+      "text": "Hypermarts"
+    },
+    {
+      "image":
+          "https://lh3.googleusercontent.com/proxy/XDXSMTZk3xQXrvg-VfEFawSBcJjthQts0W61KlI-x6k3pHO_jHjKQOjh04aXn1QpIyhBDpwX7URF05EgDiiWO_3JZoJiUCc_gA",
+      "text": "Dairy"
+    },
+    {
+      "image":
+          "https://i.pinimg.com/originals/e0/38/7b/e0387b9c6cc7a269ba92f92f8b8321ac.png",
+      "text": "Sports"
+    },
   ];
 
   @override
@@ -33,13 +61,14 @@ class Categories extends StatelessWidget {
               children: [
                 CircleAvatar(
                   radius: 25,
-                  backgroundColor: catMap[index]["color"],
+                  backgroundImage: NetworkImage(
+                    catMap[index]["image"],
+                  ),
+                  // backgroundColor: catMap[index]["color"],
                 ),
                 Text(
                   catMap[index]["text"],
-                  style: GoogleFonts.architectsDaughter(
-                    fontSize: 13
-                  ),
+                  style: GoogleFonts.architectsDaughter(fontSize: 13),
                 ),
               ],
             ),
